87e6c68f09b6118f03e7e11a3a79d554ff51a8cb
openthedidi <openthedidi2004@gmail.com>
2022-08-16 11:41:31 +0800

    add controller MockTest

---
+++ src/test/java/com/cj/cga101g1/gameplatformtype/service/GamePlatformTypeServiceImpMockTest.java
@@ -0,0 +1,57 @@
+package com.cj.cga101g1.gameplatformtype.service;
+
+import com.cj.cga101g1.gameplatformtype.dao.GamePlatformTypeDAO;
+import com.cj.cga101g1.gameplatformtype.util.GamePlatformTypeVO;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class GamePlatformTypeServiceImpMockTest {
+
+    @Autowired
+    private GamePlatformTypeService gamePlatformTypeService;
+    @MockBean
+    private GamePlatformTypeDAO gamePlatformTypeDAO;
+
+    @Test
+    void getOneType() {
+        GamePlatformTypeVO gamePlatformTypeVO_Mock = new GamePlatformTypeVO();
+        gamePlatformTypeVO_Mock.setGamePlatformNo(64007);
+        gamePlatformTypeVO_Mock.setGamePlatformName("PS3");
+
+        Mockito.when(gamePlatformTypeDAO.getType(Mockito.anyInt())).thenReturn(gamePlatformTypeVO_Mock);
+//        Mockito.when(gamePlatformTypeDAO.getType(Mockito.anyInt()))
+//                .thenThrow(new RuntimeException());
+//        Mockito.verify(gamePlatformTypeDAO,Mockito.times(2)).getType(Mockito.anyInt());
+
+        GamePlatformTypeVO gamePlatformTypeVO =
+                gamePlatformTypeService.getOneType(64002);
+        assertEquals("PS3",gamePlatformTypeVO.getGamePlatformName());
+        assertEquals(64007,gamePlatformTypeVO.getGamePlatformNo());
+    }
+
+    @Test
+    void newPlatformType() {
+    }
+
+    @Test
+    void getOneTypeByName() {
+    }
+
+    @Test
+    void getOneTypeByKeyName() {
+    }
+
+    @Test
+    void updateOneType() {
+    }
+
+    @Test
+    void deleteOneType() {
+    }
+}
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT exists `gameplatformtype` (
+                        `GamePlatformNo` int NOT NULL AUTO_INCREMENT,
+                        `GamePlatformName`  varchar(20) NOT NULL,
+                        PRIMARY KEY (`GamePlatformNo`)
+);
+
+CREATE TABLE IF NOT exists `gametype` (
+                            `GameTypeNo` int NOT NULL AUTO_INCREMENT,
+                            `GameTypeName` varchar(20) NOT NULL,
+                            PRIMARY KEY (`GameTypeNo`)
+);
+
+CREATE TABLE IF NOT exists `gamecompany` (
+                               `GameCompanyNo` int NOT NULL AUTO_INCREMENT,
+                               `GameCompanyName` varchar(20) NOT NULL,
+                               PRIMARY KEY (`GameCompanyNo`)
+);
+
+CREATE TABLE IF NOT exists `product` (
+                           `ProductNo` int NOT NULL AUTO_INCREMENT,
+                           `GameTypeNo` int NOT NULL,
+                           `GamePlatformNo` int NOT NULL,
+                           `GameCompanyNo` int NOT NULL,
+                           `ProductName` varchar(100) NOT NULL,
+                           `ProductPrice` int NOT NULL,
+                           `ProductState` int NOT NULL DEFAULT '1',
+                           `SoldTime` datetime DEFAULT NULL,
+                           `LaunchedTime` datetime DEFAULT NULL,
+                           `ItemProdDescription` varchar(1000) NOT NULL,
+                           `UpcNum` varchar(50) NOT NULL,
+                           PRIMARY KEY (`ProductNo`)
+--                            KEY `GameTypeNo_idx` (`GameTypeNo`),
+--                            KEY `GamePlatformNo_idx` (`GamePlatformNo`),
+--                            KEY `GameCompanyNo_product_idx` (`GameCompanyNo`),
+--                            CONSTRAINT `GameCompanyNo_product` FOREIGN KEY (`GameCompanyNo`) REFERENCES `gamecompany` (`GameCompanyNo`),
+--                            CONSTRAINT `GamePlatformNo_product` FOREIGN KEY (`GamePlatformNo`) REFERENCES `gameplatformtype` (`GamePlatformNo`),
+--                            CONSTRAINT `GameTypeNo_product` FOREIGN KEY (`GameTypeNo`) REFERENCES `gametype` (`GameTypeNo`)
+);

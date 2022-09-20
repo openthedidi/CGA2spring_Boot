90a964becec95d964bdb502c25920ac190c9c1d8
openthedidi <openthedidi2004@gmail.com>
2022-09-20 11:30:27 +0800

    『測試部分改用H2DB進行，修改README內容』

---
+++ target/test-classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO gameplatformtype VALUES (64001,'Switch');
+INSERT INTO gametype (gameTypeNo,gameTypeName) VALUES (63001,'動作'),(63002,'動作冒險'),(63003,'冒險	'),(63004,'大型多人在線'),(63005,'角色扮演'),(63006,'模擬'),(63007,'戰略'),(63008,'交通工具模擬'),(63009,'運動'),(63010,'射擊'),(63011,'懸疑'),(63012,'恐怖'),(63013,'好恐怖');
+INSERT INTO gamecompany (gameCompanyNo,gameCompanyName) VALUES (61001,'RockstarGames'),(61002,'Imagineer'),(61003,'2K'),(61004,'SEGA'),(61005,'Capcom'),(61006,'Sony Entertainment'),(61007,'Xbox Game Studios'),(61008,'Ubisoft'),(61009,'Saber Interactive'),(61014,'大宇資訊'),(61015,'FromSoftware'),(61016,'Quantic Dream'),(61019,'Rockstar');
+INSERT INTO product VALUES (21001,63009,64002,61003,'NBA2K21',1600,1,'2022-04-20 11:12:01','2022-04-29 09:45:10','《NBA 2K21》是世界最知名、最暢銷的《NBA 2K》系列最新發行的遊戲。具備大幅強化的頂級遊戲內容、線上競技與社群功能，以及多樣而富含深度的遊戲模式。\r\n《NBA 2K21》全方位地重現了 NBA 籃球與文化的精髓，提供獨一無二的沈浸體驗 ── 無界‧無限。','5026555427388');

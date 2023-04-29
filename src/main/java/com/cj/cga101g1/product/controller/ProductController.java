10710c4343dcf607634274cab35b459c88fea1c4
openthedidi <openthedidi2004@gmail.com>
2023-04-29 10:40:05 +0800

    『利用swagger產出API說明文件』

--- src/main/java/com/cj/cga101g1/product/controller/ProductController.java
+++ src/main/java/com/cj/cga101g1/product/controller/ProductController.java
@@ -2,8 +2,11 @@ package com.cj.cga101g1.product.controller;
 
 import com.cj.cga101g1.orderdetail.util.CartDetail;
 import com.cj.cga101g1.product.model.Product;
-import com.cj.cga101g1.product.service.Method01;
 import com.cj.cga101g1.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -11,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ import java.util.List;
 @RequestMapping("CGA101G1/product")
 @RestController
 @Validated
+@Tag(name = "product api")
 public class ProductController {
 
     @Autowired
@@ -34,6 +36,7 @@ public class ProductController {
     private ProductService productServiceImpRedisVer;
 
     @GetMapping("/OneProductDetail")
+    @Operation(summary = "單一產品詳細資訊")
     public ResponseEntity<Object> getProduct(@RequestParam Integer ProductNo){
         Object Object = productService.findByPrimaryKey(ProductNo);
        if(Object!=null){
@@ -44,25 +47,31 @@ public class ProductController {
     }
 
     @GetMapping("/showSelledCount")
+    @Operation(summary = "上架數量")
     public String showSelledCount(){
         String result = productService.showSelledCount();
         return result;
     }
     @GetMapping("/showPageProduct")
-    public ResponseEntity<List<Object>> showPageProduct(@RequestParam (defaultValue = "0") String Page){
+    @Operation(summary = "取頁數產品")
+    public ResponseEntity<List<Object>> showPageProduct(@RequestParam (defaultValue = "0", name = "頁數") String Page){
             Integer page = Integer.valueOf(Page);
             List<Object> list = productService.getAllSelledProductsByMap(page);
             return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
     @GetMapping("/showPageProduct/redis")
-    public ResponseEntity<List<Object>> showPageProductByRedis(@RequestParam (defaultValue = "0") String Page){
+    public ResponseEntity<List<Object>> showPageProductByRedis(@Parameter(description = "頁數") @RequestParam (defaultValue = "0") String Page){
         Integer page = Integer.valueOf(Page);
         List<Object> list = productServiceImpRedisVer.getAllSelledProductsByMap(page);
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
     @GetMapping("/showSellAndPlatFormTypeProductPages")
+    @Operation(summary = "取特定平台的頁數",responses = {
+            @ApiResponse(responseCode = "200", description = "回傳特定平台的總pages"),
+            @ApiResponse(responseCode = "404", description = "pages not found")
+    })
     public String showSellAndPlatFormTypeProductPages(@RequestParam Integer gamePlatformNo){
         String result = productService.showSelledCountByPlatFormType(gamePlatformNo);
         return result;
--- src/main/java/com/cj/cga101g1/product/model/Product.java
+++ src/main/java/com/cj/cga101g1/product/model/Product.java
@@ -2,7 +2,7 @@ package com.cj.cga101g1.product.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import org.hibernate.annotations.GeneratorType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -13,29 +13,42 @@ import java.sql.Timestamp;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Table(name = "product")
+@Schema(description = "商品實體類")
 public class Product extends ProductErrorMessage implements Serializable {
 //    @JsonProperty("productNumber")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "產品編號",example = "21001")
     private Integer productNo;
     @NotNull
+    @Schema(description = "遊戲類型編號")
     private Integer gameTypeNo;
     @NotNull
+    @Schema(description = "遊戲平台編號")
     private Integer gamePlatformNo;
     @NotNull
+    @Schema(description = "遊戲公司編號")
     private Integer gameCompanyNo;
     @NotNull
+    @Schema(description = "遊戲名稱")
     private String productName;
     @NotNull
+    @Schema(description = "遊戲價格")
     private Integer productPrice;
     @NotNull
+    @Schema(description = "商品狀態")
     private Integer productState;
+    @Schema(description = "商品狀態種類")
     private ProductStatusCategory productStatusCategory;
+    @Schema(description = "賣出時間")
     private Timestamp soldTime;
+    @Schema(description = "運送到達時間")
     private Timestamp launchedTime;
     @NotNull
+    @Schema(description = "商品描述")
     private String itemProdDescription;
     @NotNull
+    @Schema(description = "upcNum碼")
     private String upcNum;
 
     public Integer getProductNo() {
